e7cacfae6a4a071e83091db9a5d9d0fa14e3ea4f
Warano02 <carineteoi@gmail.com>
2025-12-09 17:12:05 +0100

    welcome page

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,21 +4,33 @@ import React from "react";
 import { Image, ImageBackground, Pressable, Text, View } from "react-native";
 
 const Welcome = () => {
-  const { logo, overlayBg, whiteCar } = assets;
+  const { logo, overlayBg, carBg } = assets;
   const router = useRouter()
   return (
-    <ImageBackground source={whiteCar} resizeMode="cover" className="flex-1 bg-white" >
+    <ImageBackground source={carBg} resizeMode="cover" className="flex-1 bg-white" >
       <ImageBackground source={overlayBg} resizeMode="cover" className="flex-1 justify-between px-5 pt-3">
         <View>
           <View className="bg-white p-3 rounded-full self-start mt-8">
             <Image source={logo}resizeMode="contain" className="w-9 h-9"/>
           </View>
           <View className="mt-8">
-            <Text className="text-white text-[40px] font-semibold">Welcome to</Text>
-            <Text className="text-white text-[40px] font-semibold">Qent</Text>
+            <Text className="text-white text-[40px] font-semibold">Lets Start</Text>
+            <Text className="text-white text-[40px] font-semibold">A New Experience</Text>
+            <Text className="text-white text-[40px] font-semibold">With Car rental.</Text>
           </View>
         </View>
-        <Pressable onPress={() => router.push("./welcome/welcome")} className="bg-button rounded-full items-center justify-center p-6 mb-8">
+        <View>
+          <Text className="font-medium text-xl text-white">
+             Discover your next adventure with Qent. we’re here to
+          </Text>
+          <Text className="font-medium text-xl text-white">
+            provide you with a seamless car rental experience.
+          </Text>
+          <Text className="font-medium text-xl text-white">
+            Let’s get started on your journey.
+          </Text>
+        </View>
+        <Pressable onPress={() => router.push("../auth")} className="bg-button rounded-full items-center justify-center p-6 mb-8">
           <Text className="text-white font-semibold text-xl">
             Next
           </Text>
