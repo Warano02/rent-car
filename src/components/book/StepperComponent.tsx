dd4d7af63e37542a63a9a89f1b6796614376b8ee
Warano02 <carineteoi@gmail.com>
2025-12-16 15:24:43 +0100

    update stepperComponents for better UX

--- src/components/book/StepperComponent.tsx
+++ src/components/book/StepperComponent.tsx
@@ -6,63 +6,43 @@ interface IStepper {
   active: number;
 }
 
-const StepperComponent = ({active = 1}: IStepper) => {
+const StepperComponent = ({ active = 3 }: IStepper) => {
+
   return (
-    <View className='flex-row items-center justify-between gap-6 py-6'>
-      <View className='bg-black absolute' style={{height:2,width:"80%",left:"10%"}} />
-      <View className='items-center justify-center ' style={{height:50}} >
-        <View className='rounded-full bg-black items-center justify-center' style={{height:20,width:20}} >
+    <View className='gap-2 justify-center'>
+      <View className='flex-row items-center pr-6 ' style={{width:"90%"}}>
+        <View className='bg-black rounded-full justify-center items-center' style={{ height: 20, width: 20 }}>
           {active > 1 && (
-            <MaterialCommunityIcons
-              name="check"
-              color={"white"}
-              size={16}
-            />
-          )}
-          {active === 1 && <View style={{height:10,width:10}} className='rounded-full bg-background' />}
+            <MaterialCommunityIcons name="check" color={"white"} size={16} />)}
+          {active === 1 && <View style={{ height: 10, width: 10 }} className='rounded-full bg-background' />}
         </View>
-        <Text
-        className='font-semibold text-[14px]'
-          style={[{marginTop:6}, active !== 1 && {color:"#7F7F7F"}]}>
+        <View className='bg-black flex-1' style={{ height: 2 }}></View>
+        <View className='bg-black rounded-full justify-center items-center' style={{ height: 20, width: 20 }}>
+          {active > 2 && (<MaterialCommunityIcons name="check" color={"white"} size={16} />)}
+          {active === 2 && <View style={{ height: 10, width: 10 }} className='rounded-full bg-background' />}
+        </View>
+        <View className='bg-black flex-1' style={{ height: 2 }}></View>
+        <View className='bg-black rounded-full justify-center items-center' style={{ height: 20, width: 20 }}>
+          {active > 3 && (<MaterialCommunityIcons name="check" color={"white"} size={16} />)}
+          {active === 3 && <View style={{ height: 10, width: 10 }} className='rounded-full bg-background' />}
+        </View>
+      </View>
+      <View className='flex-row gap-6 flex-1 h-6'>
+        <Text className={`font-semibold text-[14px] ${active !== 1 && "text-[#7F7F7F]"}`} >
           Booking details
         </Text>
-      </View>
-      <View className='items-center justify-center' style={{height:50}} >
-        <View style={{height:20,width:20}} className='bg-black rounded-full items-center justify-center'>
-          {active > 2 && (
-            <MaterialCommunityIcons
-              name="check"
-              color={"white"}
-              size={16}
-            />
-          )}
-          {active === 2 && <View style={{height:10,width:10}} className='bg-background rounded-full' />}
-        </View>
-        <Text
-         className='mt-6 text-[14px] font-semibold'
-         style={[ active !== 2 && {color:"#7F7F7F"}]}>
+        <Text className={`font-semibold text-[14px] ${active !== 2 && "text-[#7F7F7F]"}`}>
           Payment methods
         </Text>
-      </View>
-      <View className='items-center justify-center' style={{height:50}} >
-        <View style={{height:20,width:20}} className='bg-black rounded-full items-center justify-center'>
-          {active > 3 && (
-            <MaterialCommunityIcons
-              name="check"
-              color={"white"}
-              size={16}
-            />
-          )}
-          {active === 3 && <View style={{height:10,width:10}} className='bg-background rounded-full' />}
-        </View>
-        <Text
-         className='mt-6 text-[14px] font-semibold'
-         style={[ active !== 3 && {color:"#7F7F7F"}]}>
+        <Text className={`font-semibold text-[14px] ${active !== 3 && "text-[#7F7F7F]"}`}>
           confirmation
         </Text>
       </View>
-    </View>
-  );
+    </View>)
+
+
+
+
 };
 
 export default StepperComponent;
