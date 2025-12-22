6a9d20bda4f14d33fde060acda40309fb91916cc
Warano02 <carineteoi@gmail.com>
2025-12-22 19:43:27 +0100

    preparing for apk generation

--- src/components/chat/VisualizeImages.tsx
+++ src/components/chat/VisualizeImages.tsx
@@ -1,27 +1,47 @@
 import { TImageMessage } from "@/types"
-import { FlatList, Image, View } from "react-native"
+import { Dimensions, FlatList, Image, View } from "react-native"
 import { BottomSheet } from "../BottomSheet"
 
+const { width, height } = Dimensions.get("window")
+
 interface IVisualizeImages {
-    images: TImageMessage[],
-    visible: boolean,
-    setVisible: (b: boolean) => void,
-    index?: number
+  images: TImageMessage[]
+  visible: boolean
+  setVisible: (b: boolean) => void
+  index?: number
 }
-const VisualizeImages = ({ images, visible, setVisible, index }: IVisualizeImages) => {
-    return (
-        <BottomSheet visible={visible} setVisible={setVisible}>
-            <FlatList
-                data={images}
-                keyExtractor={(_, id) => id.toString()}
-                renderItem={({ item }) => (
-                    <View className='my-1'>
-                        <Image source={{ uri: item.url }} />
-                    </View>
-                )}
+
+const VisualizeImages = ({
+  images,
+  visible,
+  setVisible,
+  index = 0,
+}: IVisualizeImages) => {
+  return (
+    <BottomSheet visible={visible} setVisible={setVisible}>
+      <FlatList
+        data={images}
+        
+        pagingEnabled
+        initialScrollIndex={index}
+        keyExtractor={(_, id) => id.toString()}
+        getItemLayout={(_, i) => ({
+          length: width,
+          offset: width * i,
+          index: i,})}
+        renderItem={({ item }) => (
+          <View style={{ width, height: height * 0.5 }} className={`border-y px-1`} >
+            <Image
+              source={{ uri: item.url }}
+              style={{ width: "100%", height: "100%" }}
+              resizeMode="cover"
             />
-        </BottomSheet>
-    )
+          </View>
+        )}
+        showsHorizontalScrollIndicator={false}
+      />
+    </BottomSheet>
+  )
 }
 
 export default VisualizeImages
