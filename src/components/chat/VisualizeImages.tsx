9f81a2939b1309d6ef33d756742a61aefe27ce15
Warano02 <carineteoi@gmail.com>
2025-12-24 13:15:18 +0100

    now when u clic on image it will be show

--- src/components/chat/VisualizeImages.tsx
+++ src/components/chat/VisualizeImages.tsx
@@ -1,5 +1,6 @@
 import { TImageMessage } from "@/types"
-import { Dimensions, FlatList, Image, View } from "react-native"
+import { useState } from "react"
+import { Dimensions, FlatList, Image, Pressable, View } from "react-native"
 import { BottomSheet } from "../BottomSheet"
 
 const { width, height } = Dimensions.get("window")
@@ -17,31 +18,44 @@ const VisualizeImages = ({
   setVisible,
   index = 0,
 }: IVisualizeImages) => {
+  const [url, setUrl] = useState<string | undefined>(undefined)
   return (
     <BottomSheet visible={visible} setVisible={setVisible}>
-      <FlatList
-        data={images}
-        
-        pagingEnabled
-        initialScrollIndex={index}
-        keyExtractor={(_, id) => id.toString()}
-        getItemLayout={(_, i) => ({
-          length: width,
-          offset: width * i,
-          index: i,})}
-        renderItem={({ item }) => (
-          <View style={{ width, height: height * 0.5 }} className={`border-y px-1`} >
-            <Image
-              source={{ uri: item.url }}
-              style={{ width: "100%", height: "100%" }}
-              resizeMode="cover"
-            />
-          </View>
-        )}
-        showsHorizontalScrollIndicator={false}
-      />
+      <View>
+        <FlatList
+          data={images}
+
+          pagingEnabled
+          initialScrollIndex={index}
+          keyExtractor={(_, id) => id.toString()}
+          getItemLayout={(_, i) => ({
+            length: width,
+            offset: width * i,
+            index: i,
+          })}
+          renderItem={({ item }) => (
+            <Pressable onPress={()=>setUrl(item.url)} style={{ width, height: height * 0.5 }} className={`border-y px-1`} >
+              <Image
+                source={{ uri: item.url }}
+                style={{ width: "100%", height: "100%" }}
+                resizeMode="cover"
+              />
+            </Pressable>
+          )}
+          showsHorizontalScrollIndicator={false}
+        />
+
+        <ImageView url={url} setUrl={setUrl} />
+      </View>
     </BottomSheet>
   )
 }
 
+const ImageView = ({ url, setUrl }: { url: string | undefined, setUrl: (d: string | undefined) => void }) => {
+  return <BottomSheet visible={!!url} setVisible={() => setUrl(undefined)} >
+    <View className="flex-1">
+      <Image source={{ uri: url }} resizeMode="cover" style={{ height: "100%", width: "100%" }} />
+    </View>
+  </BottomSheet>
+}
 export default VisualizeImages
