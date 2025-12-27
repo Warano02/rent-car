798e488cf956c51dc14a514e7265aef26480f3d5
Warano02 <carineteoi@gmail.com>
2025-12-27 21:27:48 +0100

    ..

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import { Pressable, Text, View } from 'react-native'
 const Header = ({ title, end = false }: { title: string, end?: boolean }) => {
   const router = useRouter()
   return (
-    <View className='flex-row justify-between items-center mb-4 border-b-2  border-b-gray px-6'>
+    <View className='flex-row justify-between items-center pb-4 border-b  border-b-gray px-6'>
       <Pressable onPress={() => !end ? router.back() : router.replace("/main/booking/payment")} className='w-12 h-12 bg-white border border-border flex-row justify-center items-center rounded-full'>
         <Text>
           <Ionicons name="chevron-back-sharp" size={24} color={"#767676"} />
